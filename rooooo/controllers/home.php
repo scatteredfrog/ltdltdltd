ab6a43b9cf56f1c401c77393ef328ef61afa04ba
Sean Courtney <seancourtney@lavabit.com>
2014-10-12 00:07:28 -0500

    Initial commit

---
+++ rooooo/controllers/home.php
@@ -0,0 +1,14 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+	public function index()
+	{
+            $this->load->helper('url');
+            $this->load->helper('html');
+            $this->load->view('welcome_message');
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
