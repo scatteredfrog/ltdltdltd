d084507f52af3721426f21899cc133702865fcc4
Sean Courtney <seancourtney@fab4it.com>
2018-09-11 14:01:22 -0500

    ltd-b16 Fix main tile CSS background colors in mobile view

--- rooooo/views/mainmenu.php
+++ rooooo/views/mainmenu.php
@@ -11,13 +11,13 @@
             </div>
         </a>
         <a class="menuAnchor" href="/log/walk">
-            <div class="col-xs-2 col-lg-2 col-md-2 col-sm-2 menuBox" id="walk_box">
+            <div class="col-xs-2 col-lg-2 col-md-2 col-sm-2 menuBox walkBox">
                 <img src="/assets/images/menu_dogwalk.png" class="img-responsive" />
                 <div class="menuText"><span class="hidden-xs">Log a </span>Walk</div>
             </div>
         </a>
         <a class="menuAnchor" href="/log/treat">
-            <div class="col-xs-2 col-lg-2 col-md-2 col-sm-2 menuBox" id="treat_box">
+            <div class="col-xs-2 col-lg-2 col-md-2 col-sm-2 menuBox treatBox">
                 <img src="/assets/images/menu_treat.png" class="img-responsive" />
                 <div class="menuText"><span class="hidden-xs">Log a </span>Treat</div>
             </div>
@@ -27,19 +27,19 @@
     <div class="row">
         <div class="col-xs-3 col-lg-3 col-md-3 col-sm-3"></div>
         <a class="menuAnchor" href="/log/register_dog">
-            <div class="col-xs-2 col-lg-2 col-md-2 col-sm-2 menuBox" id="register_box" data-toggle="tooltip" data-placement="right" title="Register a dog to your account">
+            <div class="col-xs-2 col-lg-2 col-md-2 col-sm-2 menuBox registerBox" data-toggle="tooltip" data-placement="right" title="Register a dog to your account">
                 <img src="/assets/images/menu_registry.png" class="img-responsive" />
                 <div class="menuText"><span class="hidden-xs hidden-sm hidden-md">Dog </span>Registry</div>
             </div>
         </a>
         <a class="menuAnchor" href="/log/med">
-            <div class="col-xs-2 col-lg-2 col-md-2 col-sm-2 menuBox" id="med_box">
+            <div class="col-xs-2 col-lg-2 col-md-2 col-sm-2 menuBox medBox">
                 <img src="/assets/images/menu_meds.png" class="img-responsive" />
                 <div class="menuText"><span class="hidden-xs hidden-md hidden-sm">Log a </span>Medicine</div>
             </div>
         </a>
         <a class="menuAnchor" onclick="quickLook();">
-            <div class="col-xs-2 col-lg-2 col-md-2 col-sm-2 menuBox" id="quick_box">
+            <div class="col-xs-2 col-lg-2 col-md-2 col-sm-2 menuBox quickBox">
                 <img src="/assets/images/menu_kwik.png" class="img-responsive" />
                 <div class="menuText hidden-xs">Quick Look</div>
                 <div class="menuText hidden-sm hidden-md hidden-lg">Quick</div>
@@ -58,7 +58,7 @@
             </div>
         </a>
         <a class="menuAnchor" href="/log/walk">
-            <div class="col-xs-4 col-lg-2 col-md-3 col-sm-4 menuBox">
+            <div class="col-xs-4 col-lg-2 col-md-3 col-sm-4 menuBox walkBox">
                 <img src="/assets/images/menu_dogwalk.png" class="img-responsive" />
                 <div class="menuText">Log a Walk</div>
             </div>
@@ -68,13 +68,13 @@
     <div class="row">
         <div class="col-xs-2 col-lg-4 col-md-3 col-sm-2"></div>
         <a class="menuAnchor" href="/log/treat">
-            <div class="col-xs-4 col-lg-2 col-md-3 col-sm-4 menuBox">
+            <div class="col-xs-4 col-lg-2 col-md-3 col-sm-4 menuBox treatBox">
                 <img src="/assets/images/menu_treat.png" class="img-responsive" />
                 <div class="menuText">Log a Treat</div>
             </div>
         </a>
         <a class="menuAnchor" href="/log/register_dog">
-            <div class="col-xs-4 col-lg-2 col-md-3 col-sm-4 menuBox" data-toggle="tooltip" data-placement="right" title="Register a dog to your account">
+            <div class="col-xs-4 col-lg-2 col-md-3 col-sm-4 menuBox registerBox" data-toggle="tooltip" data-placement="right" title="Register a dog to your account">
                 <img src="/assets/images/menu_registry.png" class="img-responsive" />
                 <div class="menuText"><span class="hidden-xs">Dog </span>Registry</div>
             </div>
@@ -84,13 +84,13 @@
     <div class="row">
         <div class="col-xs-2 col-lg-4 col-md-3 col-sm-2"></div>
         <a class="menuAnchor" href="/log/med">
-            <div class="col-xs-4 col-lg-2 col-md-3 col-sm-4 menuBox">
+            <div class="col-xs-4 col-lg-2 col-md-3 col-sm-4 menuBox medBox">
                 <img src="/assets/images/menu_meds.png" class="img-responsive" />
                 <div class="menuText"><span class="hidden-xs">Log a </span>Medicine</div>
             </div>
         </a>
         <a class="menuAnchor" onclick="quickLook();">
-            <div class="col-xs-4 col-lg-2 col-md-3 col-sm-4 menuBox">
+            <div class="col-xs-4 col-lg-2 col-md-3 col-sm-4 menuBox quickBox">
                 <img src="/assets/images/menu_kwik.png" class="img-responsive" />
                 <div class="menuText">Quick Look</div>
             </div>
